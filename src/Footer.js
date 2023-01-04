b6bebb038e869a63666073853fbae1d52de2e218
Brandi Ball <brandiball@Brandis-MBP.attlocal.net>
2023-01-03 20:35:21 -0500

    sketched out landing page basics

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer (props) {
+    return (
+        <>
+        <div> Bakery in Paris </div>
+        <div className='links'>
+            <div> About </div>
+            <div> Menu </div>
+            <div> Location</div>
+            <div> Social Media </div>
+        </div>
+        
+        </>
+    )
+}
--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,13 @@ export default function Header (props) {
     return (
         <>
         <div> Bakery in Paris </div>
-        <div> Menu </div>
-        <div> Location</div>
-        <div> Social Media </div>
+        <div className='links'>
+            <div> About </div>
+            <div> Menu </div>
+            <div> Location</div>
+            <div> Social Media </div>
+        </div>
+        
         </>
     )
 }
