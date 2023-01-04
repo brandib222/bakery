49d1367dc66a5f19c5c313cc0028e4cd504ba552
Brandi Ball <brandiball@Brandis-MBP.attlocal.net>
2023-01-03 21:41:51 -0500

    finished landing page sketch

--- src/Footer.js
+++ src/Footer.js
@@ -2,14 +2,11 @@ import React from "react";
 
 export default function Footer (props) {
     return (
-        <div className='footer'>
-            <div className='links'>
-                <h2> Bakery in Paris </h2>
-                <div> About </div>
-                <div> Menu </div>
-                <div> Location</div>
-                <div> Social Media </div>
+            <div className='footer'>
+                    <div> About </div>
+                    <div> Menu </div>
+                    <div> Location</div>
+                    <div> Social Media </div>
             </div>
-        </div>
     )
 }
