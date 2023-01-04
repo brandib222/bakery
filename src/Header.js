0dc48ae786cbe55d0080b6b5ebddb5e756bca928
Brandi Ball <brandiball@Brandis-MBP.attlocal.net>
2023-01-03 20:14:01 -0500

    set up landing page, header and footer

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ export default function Header (props) {
     return (
         <>
         <div> Bakery in Paris </div>
+        <div> Menu </div>
+        <div> Location</div>
+        <div> Social Media </div>
         </>
     )
 }
